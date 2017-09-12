57ced5e02d8036620120ceb27b06a20c0527898c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-12 15:22:23 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -55,7 +55,7 @@ describe('Arrays', function() {
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
     })
   })
-/*
+
   describe('removeLastKitten()', function() {
     it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
       expect(removeLastKitten()).toEqual(["Milo", "Otis"])
@@ -63,7 +63,7 @@ describe('Arrays', function() {
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
     })
   })
-
+/*
   describe('removeFirstKitten()', function() {
     it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
       expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
