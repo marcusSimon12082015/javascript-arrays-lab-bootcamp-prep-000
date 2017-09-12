09e8ff455c58578152d56419e5df25f44dfc9084
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-12 15:15:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function appendKitten(name){
   return newArray
 }
 function prependKitten(name){
-    var newArray = kittens.clone()
+    var newArray = kittens.slice
     newArray.unshift(name)
     return newArray
 }
